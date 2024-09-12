cf038fb5950867a6c0b67f77a100f5c6098eea4f
Khrystyna Yelyseyeva <play1409@gmail.com>
2024-09-12 16:42:51 +0300

    add custom pipe for active todos, make separated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Todo } from './types/todo';
 
+const todos = [
+  { id: 1, title: 'HTML + CSS', completed: true },
+  { id: 2, title: 'JS', completed: false },
+  { id: 3, title: 'React', completed: false },
+  { id: 4, title: 'Vue', completed: false },
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-v16-todo-app';
+  todos = todos;
+
+  trackById(i: number, todo: Todo) {
+    return todo.id;
+  }
+
+  addTodo(title: string) {
+    const newTodo: Todo = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+
+    this.todos = [...this.todos, newTodo];
+  }
+
+  renameTodo(todoId: number, title: string) {
+    this.todos = this.todos.map((todo) => {
+      if (todoId === todo.id) {
+        return { ...todo, title };
+      }
+
+      return todo;
+    });
+  }
+
+  completeTodo(todoId: number) {
+    this.todos = this.todos.map((todo) => {
+      if (todoId === todo.id) {
+        return { ...todo, completed: !todo.completed };
+      }
+
+      return todo;
+    });
+  }
+
+  deleteTodo(todoId: number) {
+    this.todos = this.todos.filter((todo) => todoId !== todo.id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TodoComponent } from './components/todo/todo.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { FilterActivePipe } from './pipes/filter-active.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoComponent, TodoFormComponent, FilterActivePipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
